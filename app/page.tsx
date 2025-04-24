2d813b4bc1554c7adb09c8aef8042eedcc015d06
Khit Samno <khitsamno@gmail.com>
2025-04-24 22:39:43 +0700

    init project from local

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,122 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { createWorker } from "tesseract.js";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { CheckCircle2, Loader } from "lucide-react";
 
 export default function Home() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [ocrResult, setOcrResult] = useState<string>("");
+  const [ocrStatus, setOcrStatus] = useState<string>("");
+  const [lang, setLang] = useState("eng");
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setSelectedImage(event.target.files[0]);
+      setOcrResult(""); // Reset OCR result
+      setOcrStatus(""); // Reset status
+    }
+  };
+
+  const readImageText = async () => {
+    if (!selectedImage) return;
+
+    setOcrStatus("Processing");
+    const worker = await createWorker(["eng", "khm", "chi_sim"], 1, {
+      logger: (m) => console.log(m), // Add logger here
+    });
+
+    try {
+      const {
+        data: { text },
+      } = await worker.recognize(selectedImage);
+
+      setOcrResult(text);
+      setOcrStatus("Completed");
+    } catch (error) {
+      console.error(error);
+      setOcrStatus("Error occurred during processing.");
+    } finally {
+      await worker.terminate();
+    }
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <h1 className="text-2xl font-bold  border-b-4 border-green-500">
+          Next Image Capture
+        </h1>
+        <div className="w-[450px] rounded-md shadow p-4">
+          <div className=" space-y-1">
+            <h1 className="text-xl font-bold">Documents</h1>
+            <h4 className="text-sm font-medium text-gray-500">
+              Upload employee identification and work documents
+            </h4>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+          <div className="grid w-full items-center gap-1.5 mt-7">
+            <Label htmlFor="picture">Picture of document</Label>
+            <Input
+              id="picture"
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="w-full"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          {selectedImage && (
+            <div className="p-3 border border-gray-200 mt-4 rounded">
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                alt="Uploaded content"
+                className="rounded"
+              />
+            </div>
+          )}
+          <div className="mt-4">
+            <Button onClick={readImageText}>Execute</Button>
+          </div>
+
+          <div className="border-t mt-5">
+            {ocrStatus && (
+              <div className=" px-2 py-1 rounded-md border border-slate-200 mt-3">
+                <div className="text-xs font-medium flex items-center space-x-1.5">
+                  {ocrStatus === "Processing" ? (
+                    <Loader className=" size-3 animate-spin" />
+                  ) : (
+                    <CheckCircle2 className=" size-3.5 text-green-500" />
+                  )}
+
+                  <span className=" font-medium text-gray-500">
+                    {ocrStatus}
+                  </span>
+                </div>
+              </div>
+            )}
+            {ocrResult && (
+              <div>
+                <h3 className="font-semibold mt-5 text-sm">Text Extracted</h3>
+                <div className="py-1 px-2 rounded-md bg-slate-50 border border-slate-100 mt-2">
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: ocrResult
+                        .replace(/\n/g, "<br />")
+                        .replace(/[=,—,-,+]/g, " "),
+                    }}
+                    style={{
+                      width: "fit-content",
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
